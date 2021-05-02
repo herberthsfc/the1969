b99698577b56fa7048fee9fcac1c31269b00997b
herberthsfc <76819767+herberthsfc@users.noreply.github.com>
2021-05-02 08:18:27 -0300

    Add files via upload

---
+++ language/nad.js
@@ -0,0 +1,7 @@
+exports.baned = () => {
+	return`*❬❗️❭ Por floodar, ou descumprir alguma regra, voce não pode usar o bot no momento!*`
+}
+
+exports.premium = () => {
+	return`*❬💎️❭ Você não é um membro Premium, digite ${prefix}serpremium para comprar acesso Premium!*`
+}
